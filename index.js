7c83e33288d020b4c97e4da7cebb534100d8b7b4
Cayla Schwartz <cayschwartz@gmail.com>
2017-08-29 16:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var name = "Joe"
 var height = "74"
 
+if name === "susan"{
+
+  console.log("the name is Susan")
+}
+else{
+  console.log("this isn't susan")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
